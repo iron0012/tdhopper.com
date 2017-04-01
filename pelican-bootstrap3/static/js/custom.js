1c5948440ce4a94f94b22187c912c5008b70395e
Tim Hopper <tdhopper@gmail.com>
2017-04-01 09:26:34 -0400

    add bigfoot

--- pelican-bootstrap3/static/js/custom.js
+++ pelican-bootstrap3/static/js/custom.js
@@ -1,3 +1,11 @@
   $(document).ready(function(){
         $('div.cell div.prompt').hide(); // Hide jupyter cell numbers
   });
+
+    var bigfoot = $.bigfoot(
+        {
+            deleteOnUnhover: false,
+            preventPageScroll: false,
+            hoverDelay: 250
+        }
+    );
