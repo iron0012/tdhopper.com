8226911dada65927376d051786cc36ed0b1f3077
Tim Hopper <tdhopper@gmail.com>
2017-04-24 15:14:23 -0400

    pretttier home

--- pelican-bootstrap3/static/js/custom.js
+++ pelican-bootstrap3/static/js/custom.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
         setTimeout(function(){
             $('body').scrollspy('refresh');
         },1000);
-    }
+    };
 
     /*============================================
     Refresh waypoints function
@@ -105,6 +105,19 @@ $(document).ready(function() {
         setTimeout(function(){
             $.waypoints('refresh');
         },1000);
-    }
+    };
+
+
+    /*============================================
+    ScrollTo Links
+    ==============================================*/
+    $('a.scrollto').click(function(e){
+        $('html,body').scrollTo(this.hash, this.hash, {gap:{y:-50},animation:  {easing: 'easeInOutCubic', duration: 1600}});
+        e.preventDefault();
+
+        if ($('.navbar-collapse').hasClass('in')){
+            $('.navbar-collapse').removeClass('in').addClass('collapse');
+        }
+    });
 
 });
